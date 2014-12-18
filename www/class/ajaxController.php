70bc02061022ef037222c06a5f3835964b5c3ab6
Kolyabis <2z.dizayn@gmail.com>
2014-12-18 16:26:20 +0200

    Файл проверки на jQuery плюс отправка данных ajax

--- www/class/ajaxController.php
+++ www/class/ajaxController.php
@@ -28,14 +28,27 @@ class ajaxController{
 			echo "Пользователь: (".$name.") успешно добавлен!";
 		}
 	}
+    /* Метод проверки list сообщения */
+    public function addListComent($txt, $token, $id, $login, $db){
+        $txt = filter_var($txt);
+        if($db->query("INSERT INTO list (`id_user`,`login`,`key`,`text`,`data`,`persona`) VALUES ($id,'$login','$token','$txt', NOW(), 'user')")){
+            echo("INSERT INTO list (`id_user`,`login`,`key`,`text`,`data`,`persona`)VALUES ($id,'$login','$token','$txt', NOW(), 'user')");
+        }
+    }
 }
 $ajax = new ajaxController();
 	/*Проверяем существует ли пользователь или даем форму регистрации */
 	if(isset($_POST['login']) and isset($_POST['pass']) and !empty($_POST['login']) and !empty($_POST['pass'])){
 		$ajax->checkUser($_POST['login'], $_POST['pass'], $db);
 	}
+
 	/* Метод добовления нового пользователя */
 	if(isset($_POST['name']) and !empty($_POST['name'])){
 		$ajax->insertUser($_POST['key'], $_POST['name'], $_POST['pass'], $_POST['edrpo'], $_POST['mail'], $_POST['tel'], $db);
 	}
+
+    /* Метод проверки list сообщения */
+    if(isset($_POST['txt']) and isset($_POST['token']) and isset($_POST['id']) and isset($_POST['login'])){
+        $ajax->addListComent($_POST['txt'], $_POST['token'], $_POST['id'], $_POST['login'], $db);
+    }
 ?>
